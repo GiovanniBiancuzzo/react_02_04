1c52350ef902e65e856c4d49224c99f0058b9e57
Stefano Casasola <ste.casasola@gmail.com>
2022-03-31 17:23:26 +0200

    App update

--- src/components/CompanySearchResults.jsx
+++ src/components/CompanySearchResults.jsx
@@ -1,37 +1,36 @@
 import { useEffect, useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import Job from './Job'
-import uniqid from 'uniqid'
 import { useParams } from 'react-router-dom'
 
 const CompanySearchResults = () => {
+  const [jobs, setJobs] = useState([])
+  const params = useParams()
 
-    const [jobs, setJobs] = useState([])
-    const params = useParams()
+  useEffect(() => {
+    getJobs()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-    useEffect(() => {
-        getJobs()
-    }, [])
+  const baseEndpoint = 'https://strive-jobs-api.herokuapp.com/jobs?company='
 
+  const getJobs = async () => {
+    const response = await fetch(baseEndpoint + params.companyName)
+    const { data } = await response.json()
+    setJobs(data)
+  }
 
-    const baseEndpoint = 'https://strive-jobs-api.herokuapp.com/jobs?company='
-
-    const getJobs = async () => {
-        const response = await fetch(baseEndpoint + params.companyName)
-        const { data } = await response.json()
-
-        setJobs(data)
-    }
-
-    return (
-        <Container>
-            <Row>
-                <Col>
-                    {jobs.map(jobData => <Job key={uniqid()} data={jobData} />)}
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container>
+      <Row>
+        <Col>
+          {jobs.map((jobData) => (
+            <Job key={jobData._id} data={jobData} />
+          ))}
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default CompanySearchResults
--- src/components/MainSearch.jsx
+++ src/components/MainSearch.jsx
@@ -1,59 +1,62 @@
 import { Component } from 'react'
 import { Container, Row, Col, Form } from 'react-bootstrap'
 import Job from './Job'
-import uniqid from 'uniqid'
 
 class MainSearch extends Component {
+  state = {
+    query: '',
+    jobs: [],
+  }
 
-    state = {
-        query: '',
-        jobs: []
+  baseEndpoint = 'https://strive-jobs-api.herokuapp.com/jobs?search='
+
+  handleChange = (e) => {
+    this.setState({ query: e.target.value })
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const response = await fetch(
+      this.baseEndpoint + this.state.query + '&limit=20'
+    )
+
+    if (!response.ok) {
+      alert('Error fetching results')
+      return
     }
 
-    baseEndpoint = 'https://strive-jobs-api.herokuapp.com/jobs?search='
+    const { data } = await response.json()
 
+    this.setState({ jobs: data })
+  }
 
-    handleChange = (e) => {
-        this.setState({ query: e.target.value })
-    }
-
-    handleSubmit = async (e) => {
-        e.preventDefault()
-
-        const response = await fetch(this.baseEndpoint + this.state.query + '&limit=20')
-
-        if (!response.ok) {
-            alert('Error fetching results')
-            return
-        }
-
-        const { data } = await response.json()
-
-        this.setState({ jobs: data })
-
-    }
-
-    render() {
-        return (
-            <Container>
-                <Row>
-                    <Col xs={10} className='mx-auto my-3'>
-                        <h1>Remote Jobs Search</h1>
-                    </Col>
-                    <Col xs={10} className='mx-auto'>
-                        <Form onSubmit={this.handleSubmit}>
-                            <Form.Control type="search" value={this.state.query} onChange={this.handleChange} placeholder="type and press Enter" />
-                        </Form>
-                    </Col>
-                    <Col xs={10} className='mx-auto mb-5'>
-                        {
-                            this.state.jobs.map(jobData => <Job key={uniqid()} data={jobData} />)
-                        }
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col xs={10} className="mx-auto my-3">
+            <h1>Remote Jobs Search</h1>
+          </Col>
+          <Col xs={10} className="mx-auto">
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Control
+                type="search"
+                value={this.state.query}
+                onChange={this.handleChange}
+                placeholder="type and press Enter"
+              />
+            </Form>
+          </Col>
+          <Col xs={10} className="mx-auto mb-5">
+            {this.state.jobs.map((jobData) => (
+              <Job key={jobData._id} data={jobData} />
+            ))}
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
 export default MainSearch
